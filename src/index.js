6a65d3f406f1a4bcb656fbe2e94012db016735bc
Roman <65686226+morozit@users.noreply.github.com>
2020-11-21 01:36:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,2 @@
 import './SASS/main.scss';
-
-
-import { startBtnClickHandler, stopBtnClickHandler } from './js/start-stop';
-
-
-const startBtn = document.querySelector('[data-action="start"]');
-const stopBtn = document.querySelector('[data-action="stop"]');
-
-startBtn.addEventListener('click', startBtnClickHandler);
-stopBtn.addEventListener('click', stopBtnClickHandler);
+import './JS/change-color.js';
